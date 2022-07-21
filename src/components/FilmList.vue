6b7490b2eb43b7fb0828dc7d61812a1d029f68bb
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-21 19:09:45 +0200

    Feat:pictures showing

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="row justify-content-center">
     <h2>Movies</h2>
-     <ul v-for="(item, index) in MovieArray" :key="index" class="col-2 bg-primary mx-2">
+     <ul v-for="(item, index) in MovieArray" :key="index" class="col-3 bg-primary mx-2 ">
+        <li><img :src="imgUrl + item.poster_path" alt=""></li> 
         <li>
             {{item.title}}
         </li>
@@ -26,7 +27,7 @@ import LanguageFlag from './LanguageFlag.vue';
 export default {
     data() {
         return {
-            
+            imgUrl: 'https://image.tmdb.org/t/p/w342/'
         }
     },
     components:{
@@ -39,5 +40,10 @@ export default {
 </script>
 
 <style lang='scss'>
-
+li{
+    
+    img{
+        width: 100%;
+    }
+}
 </style>
